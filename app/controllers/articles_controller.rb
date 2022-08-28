58657b9a575373884ab8bea38d0a55265edb767e
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-08-28 22:34:49 +0200

    Project finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dev", password: "12345", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
@@ -39,11 +42,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path, status: :see_other
+    redirect_to articles_path, status: :see_other
   end
 
-  private
+    private
     def article_params
       params.require(:article).permit(:title, :body, :status)
     end
+
 end
